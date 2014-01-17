017a1c80009c2710fe2c5712b18765bb7c8be127
Fedor Iskhakov <f@iskh.ru>
2014-01-17 16:53:19 +1100

    Differences in deaton 2 model

--- egdst_examples/model_deaton2.m
+++ egdst_examples/model_deaton2.m
@@ -1,4 +1,4 @@
-%Deterministic deaton model with interest=discount and credit constraint a0==0
+%Stochastic deaton model with interest=discount and credit constraint a0<0
 
 delete out.txt %diary
 diary out.txt %diary
@@ -8,11 +8,11 @@ deaton1=egdstmodel('deaton1','tmp_deaton1');
 deaton1.t0=1;             %initial period
 deaton1.T=25;             %terminal period
 %grids
-deaton1.mmax=50;          %max cash-on-hand
+deaton1.mmax=100;          %max cash-on-hand
 deaton1.ngridmax=1000;    %max number of grid point in assets
 deaton1.ngridm=100;       %standard number of grid points in assets
 deaton1.nthrhmax=10;      %max number of treshold points in TH
-deaton1.ny=2;             %number of points in discrete representation of income shocks
+deaton1.ny=10;             %number of points in discrete representation of income shocks
 %states and discrete choices
 deaton1.s={'Singleton state',{0,'dummy state'}};
 deaton1.trpr={'true',[1]};
@@ -33,10 +33,10 @@ deaton1.param={'interest','return on savings',0.01};
 deaton1.eq={'income_level','Realized income','income*shock','next'};
 deaton1.param={'income','income (times multiplicator shock)',1.25};
 %credit constraint
-deaton1.a0=0;
+deaton1.a0=-25;
 %shock (no shock)
 deaton1.shock='lognormal';
-deaton1.shock={'sigma','0'};
+deaton1.shock={'sigma','0.75'};
 deaton1.shock={'mu','0'};
 
 %Verbosity
@@ -54,9 +54,9 @@ catch er
 end
 
 %Simulate and plot
-deaton1.sim([1 8.0]);
+deaton1.sim([1 0.25]);
 try
-    deaton1.plot2('mack');
+    deaton1.plot2('mack-q1');
 catch er
 end
 
