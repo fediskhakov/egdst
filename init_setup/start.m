a2d702779b7938ec6e9f7842c973505ddcc26a6f
Fedor Iskhakov <f@iskh.ru>
2014-01-22 14:43:26 +0100

    Update to test code

--- init_setup/start.m
+++ init_setup/start.m
@@ -3,10 +3,16 @@
 try
 	mex test.c CFLAGS='\$CFLAGS -fopenmp' LDFLAGS='\$LDFLAGS -fopenmp'
 	test
-	fprintf('^^^^^^ check for OpenMP message\n\nMex is configured properly!\n');
+	fprintf('^^^^^^ check for OpenMP message\nMex is configured properly!\n');
 	if ismac 
 		!say Ready to rock!
 	end
 catch errr
-	fprintf('Mex failed! Run mex test.c to see the error message itself\n');
+	try
+		mex test.c
+		test
+		fprintf('OpenMP support not available.\nOtherwise, mex is configured properly!\n');
+	catch errr
+		fprintf('Mex failed! Run mex test.c to see the error message itself\n');
+	end
 end
--- init_setup/test.c
+++ init_setup/test.c
@@ -11,6 +11,9 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
 	printf("Test mex function was successfully created!\n");
 	printf("Number of inputs: %d\nNumber of outputs: %d\n",nrhs,nlhs);
 	printf("Compiled on %s %s\n",__DATE__,__TIME__);
+#ifdef __GNUC__
+	printf("GCC version %d.%d.%d\n",__GNUC__, __GNUC_MINOR__,__GNUC_PATCHLEVEL__);
+#endif
     
 #ifdef _OPENMP
     printf("OpenMP is avaliable with %d cores\n", omp_get_num_procs());
